8516f402cf93a2f0cc7694df650a2d87a44bf56c
Manish Thota <127055778+thota25@users.noreply.github.com>
2026-09-06 19:44:46 -0400

    Updated portfolio title in layout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,14 @@
+import './globals.css';
+
 export const metadata = {
-  title: 'Portfolio',
+  title: 'Manish Thota - Portfolio',
   description: 'Personal Portfolio Website',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
