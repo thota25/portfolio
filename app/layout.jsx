ab8fe0ba2796de2b69809a62650781287a542359
Manish Thota <127055778+thota25@users.noreply.github.com>
2026-09-07 20:55:53 -0400

    Added Tailwind CSS script and update body class

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,3 @@
-import './globals.css';
-
 export const metadata = {
   title: 'Manish Thota - Portfolio',
   description: 'Personal Portfolio Website',
@@ -8,7 +6,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="bg-slate-950 text-slate-100">{children}</body>
     </html>
   );
 }
